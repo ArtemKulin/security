415cd7ba642e39ddc938d271d5de20a46a2efabc
Artem Kulin <kulinartem@gmail.com>
2021-02-05 21:18:35 +0300

    add Dao interface

---
+++ src/main/java/ru/kulinartem/security/dao/Dao.java
@@ -0,0 +1,18 @@
+package ru.kulinartem.security.dao;
+
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    public T getItemById(long id);
+
+    public List<T> getAllItems();
+
+    public void saveItem(T item);
+
+    public void deleteItem(T item);
+
+    public void updateItem(T item, long id);
+
+}
