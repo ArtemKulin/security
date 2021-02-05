e46cc9bc7a4f111c2b224fdc02f8df34c7088518
Artem Kulin <kulinartem@gmail.com>
2021-02-05 18:42:33 +0300

    add Web config file

--- src/main/java/ru/kulinartem/security/config/Web.java
+++ src/main/java/ru/kulinartem/security/config/Web.java
@@ -1,2 +1,53 @@
-package ru.kulinartem.security.config;public class Web {
+package ru.kulinartem.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "ru.kulinartem.security")
+@EnableWebMvc
+public class Web implements WebMvcConfigurer {
+
+    private final ApplicationContext context;
+
+    @Autowired
+    public Web(ApplicationContext context) {
+        this.context = context;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setApplicationContext(context);
+        templateResolver.setPrefix("/WEB-INF/view/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setContentType("text/html; charset=UTF-8");
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
+    }
 }
