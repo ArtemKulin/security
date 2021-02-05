1ac989a229eb9620fa6099e8b12a481568816bd3
Artem Kulin <kulinartem@gmail.com>
2021-02-05 21:01:06 +0300

    modify Hibernate config: rework hibernatePropeties method

--- src/main/java/ru/kulinartem/security/config/Hibernate.java
+++ src/main/java/ru/kulinartem/security/config/Hibernate.java
@@ -53,9 +53,9 @@ public class Hibernate {
     @Bean
     public Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.put("hibernate.dialect", "hibernate.dialect");
-        properties.put("hibernate.show_sql", "hibernate.show_sql");
-        properties.put("hibernate.hbm2ddl.auto", "hibernate.hbm2ddl.auto");
+        properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
         return properties;
     }
 
