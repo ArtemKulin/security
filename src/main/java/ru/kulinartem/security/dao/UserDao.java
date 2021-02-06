2202ec3be4eb75520039516ce6e1c7a479cf0ad2
Artem Kulin <kulinartem@gmail.com>
2021-02-06 13:48:40 +0300

    rework UserDao: change class to interface

--- src/main/java/ru/kulinartem/security/dao/UserDao.java
+++ src/main/java/ru/kulinartem/security/dao/UserDao.java
@@ -1,48 +1,23 @@
 package ru.kulinartem.security.dao;
 
-import org.springframework.stereotype.Repository;
 import ru.kulinartem.security.model.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
-public class UserDao implements Dao<User>{
-
-    @PersistenceContext
-    private EntityManager manager;
+public interface UserDao extends Dao<User>{
 
     @Override
-    public User getItemById(long id) {
-        return manager.find(User.class, id);
-    }
+    public User getItemById(long id);
 
     @Override
-    @SuppressWarnings("unchecked")
-    public List<User> getAllItems() {
-        return  manager.createQuery("FROM User")
-                .getResultList();
-    }
+    public List<User> getAllItems();
 
     @Override
-    public void saveItem(User item) {
-        manager.persist(item);
-    }
+    public void saveItem(User item);
 
     @Override
-    public void deleteItem(User item) {
-        User deletedUser = manager.find(User.class, item.getId());
-        manager.remove(deletedUser);
-    }
+    public void deleteItem(User item);
 
     @Override
-    public void updateItem(User item, long id) {
-        User updatedUser = getItemById(id);
-        updatedUser.setName(item.getName());
-        updatedUser.setLastName(item.getLastName());
-        updatedUser.setAge(item.getAge());
-        updatedUser.setEmail(item.getEmail());
-        manager.merge(updatedUser);
-    }
+    public void updateItem(User item, long id);
 }
