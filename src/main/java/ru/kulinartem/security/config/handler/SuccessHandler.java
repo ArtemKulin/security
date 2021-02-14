45586ba6101f536fde2e85dd22bb166413830d9c
Artem Kulin <kulinartem@gmail.com>
2021-02-14 22:53:33 +0300

    modify success handler file

--- src/main/java/ru/kulinartem/security/config/handler/SuccessHandler.java
+++ src/main/java/ru/kulinartem/security/config/handler/SuccessHandler.java
@@ -1,12 +1,11 @@
 package ru.kulinartem.security.config.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import ru.kulinartem.security.dao.UserDao;
-import ru.kulinartem.security.dao.UserDaoImpl;
-import ru.kulinartem.security.model.User;
+import ru.kulinartem.security.service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +15,23 @@ import java.util.Set;
 
 @Component
 public class SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final UserService userService;
+
+    @Autowired
+    public SuccessHandler(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        String name = httpServletRequest.getParameter("email");
+        long id = userService.getItemByEmail(name).getId();
         if (roles.contains("ROLE_ADMIN")) {
-            httpServletResponse.sendRedirect("/");
+            httpServletResponse.sendRedirect("/admin/");
         } else {
-            httpServletResponse.sendRedirect("/user");
+            httpServletResponse.sendRedirect("/user/" + id);
         }
     }
 }
