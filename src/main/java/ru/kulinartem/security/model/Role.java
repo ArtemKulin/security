5a6ba16cf2dbbc1d7e5bea94444962b05c5c7404
Artem Kulin <kulinartem@gmail.com>
2021-02-18 18:27:20 +0300

    rework Role model

--- src/main/java/ru/kulinartem/security/model/Role.java
+++ src/main/java/ru/kulinartem/security/model/Role.java
@@ -3,9 +3,8 @@ package ru.kulinartem.security.model;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
+
 
 @Entity
 @Table (name = "roles")
@@ -13,35 +12,17 @@ public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (insertable = false)
     private long id;
 
-    @Column (name = "role")
+   @Column (name = "role")
     private String role;
-
-    @ManyToMany (
-            cascade = {
-            CascadeType.PERSIST, CascadeType.MERGE
-            , CascadeType.REFRESH, CascadeType.DETACH}
-            , fetch = FetchType.LAZY)
-    @JoinTable (name = "users_roles"
-            , joinColumns = @JoinColumn (name = "role_id")
-            , inverseJoinColumns = @JoinColumn (name = "user_id")
-            )
-    private List<User> users;
 
     public Role() {
     }
 
     public Role(String role) {
         this.role = role;
-    }
-
-    public void addUserToRole(User user) {
-        if (users.isEmpty()) {
-            users = new ArrayList<>();
-        } else {
-            users.add(user);
-        }
     }
 
     @Override
@@ -77,11 +58,11 @@ public class Role implements GrantedAuthority {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Role role1 = (Role) o;
-        return id == role1.id && role.equals(role1.role) && Objects.equals(users, role1.users);
+        return id == role1.id && Objects.equals(role, role1.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, role, users);
+        return Objects.hash(id, role);
     }
 }
